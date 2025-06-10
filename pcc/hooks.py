f8c94bf4c56774a35ae8bc4dc40afe89618f401c
AravindR97 <aravindrajeshchry@gmail.com>
2025-06-10 11:43:23 +0530

    feat(Job Record): Create Sales Order with pending items from Job Record

--- pcc/hooks.py
+++ pcc/hooks.py
@@ -29,7 +29,8 @@ app_license = "mit"
 
 # include js in doctype views
 doctype_js = {
-    "Purchase Order": "public/js/purchase_order.js"
+    "Purchase Order": "public/js/purchase_order.js",
+    "Sales Order": "public/js/sales_order.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
